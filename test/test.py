b73c9b84e1da9c0177a06fa104a1fd33915e9f46
student <prachiege2025@gmail.com>
2026-02-04 14:15:23 +0530

    added the src code along with test cases and also added the pytest workflow yml files

---
+++ test/test.py
@@ -0,0 +1,31 @@
+import pytest
+from code import calc_avg, grade
+
+
+def test_calc_avg():
+    marks = [80, 90, 70, 85, 75]
+    assert calc_avg(marks) == 80
+
+
+def test_average_requires_five_subjects():
+    with pytest.raises(ValueError):
+        calc_avg([80, 90, 70])  # Less than 5 subjects
+
+
+@pytest.mark.parametrize(
+    "average, expected_grade",
+    [
+        (95, "A+"),
+        (90, "A+"),
+        (80, "A"),
+        (75, "A"),
+        (65, "B"),
+        (60, "B"),
+        (55, "C"),
+        (50, "C"),
+        (45, "Fail"),
+        (30, "Fail"),
+    ],
+)
+def test_grade(average, expected_grade):
+    assert grade(average) == expected_grade
